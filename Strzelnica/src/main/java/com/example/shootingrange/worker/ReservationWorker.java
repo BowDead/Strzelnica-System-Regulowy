43c6158bbc82d0896f00c632ee7e8c12868fd916
capSaymon <szymonpietruszka@onet.pl>
2026-01-14 23:19:53 +0100

    sprawdzenie dostępności daty

--- Strzelnica/src/main/java/com/example/shootingrange/worker/ReservationWorker.java
+++ Strzelnica/src/main/java/com/example/shootingrange/worker/ReservationWorker.java
@@ -1,20 +1,43 @@
 package com.example.shootingrange.worker;
 
 import io.camunda.client.annotation.JobWorker;
+import io.camunda.client.annotation.Variable;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class ReservationWorker {
 
     @JobWorker(type = "searchDateToReservation")
-    public Map<String, Object> searchDate() {
-        boolean available = true;
-        System.out.println("Dostępna data rezerwacji: " + available);
-        return Map.of("dateAvailable", available);
+    public Map<String, Object> searchDateToReservation(
+            @Variable(name = "sessionDate") String sessionDateStr){
+
+        Map<String, Object> result = new HashMap<>();
+        LocalDate sessionDate = LocalDate.parse(sessionDateStr);
+        boolean dateAvailable = false;
+        boolean clientAcceptsDate = Math.random() < 0.5;
+
+        if (dateAvailable == false && clientAcceptsDate) {
+            System.out.println("Data niedostępna [data: " + sessionDate + "]");
+            sessionDate = sessionDate.plusDays(1);
+        }
+        else {
+            System.out.println("Data dostępna: " + sessionDate + "\n");
+            dateAvailable = true;
+        }
+
+        result.put("dateAvailable", dateAvailable);
+        result.put("sessionDate", sessionDate.toString());
+
+        return result;
     }
 
+
+
+    // Decyzja o rezerwacji
     @JobWorker(type = "decideAboutReservation")
     public Map<String, Object> decideAboutReservation() {
         boolean accept = true;
@@ -22,22 +45,25 @@ public class ReservationWorker {
         return Map.of("accept", accept);
     }
 
+    // Autoryzacja płatności
     @JobWorker(type = "authorize-payment")
     public void authorizePayment() {
-        System.out.println("Pomyślna autoryzacja zapłaty za sesje");
+        System.out.println("Pomyślna autoryzacja zapłaty za sesję");
     }
 
-    @JobWorker(type = "check_positions_and_instructors")
+    // Sprawdzenie dostępności pozycji i instruktorów
+    @JobWorker(type = "checkAvailablePositionsAndInstructors")
     public Map<String, Object> checkPositionsAndInstructors() {
         boolean positionsAndInstructors = true;
         System.out.println("Przydzielenie pozycji i instruktora: " + positionsAndInstructors);
         return Map.of("positionsAndInstructors", positionsAndInstructors);
     }
 
+    // Sprawdzenie akcji klienta
     @JobWorker(type = "client_action_check")
     public Map<String, Object> clientActionCheck() {
         boolean clientActionAccepted = true;
-        System.out.println("Akcja klienta: " + clientActionAccepted);
+        System.out.println("Akcja klienta zaakceptowana: " + clientActionAccepted);
         return Map.of("clientActionAccepted", clientActionAccepted);
     }
 }
