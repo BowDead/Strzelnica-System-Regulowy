c45613aff2075b054d29de44ff8755ca4edda44c
capSaymon <szymonpietruszka@onet.pl>
2026-01-14 16:47:10 +0100

    usunięcie sprawdzenia wieku

--- Strzelnica/src/main/java/com/example/shootingrange/worker/RegistrationWorkers.java
+++ Strzelnica/src/main/java/com/example/shootingrange/worker/RegistrationWorkers.java
@@ -1,6 +1,5 @@
 package com.example.shootingrange.worker;
 
-
 import io.camunda.client.annotation.JobWorker;
 import io.camunda.client.annotation.Variable;
 import org.springframework.stereotype.Component;
@@ -9,9 +8,6 @@ import java.util.Map;
 
 @Component
 public class RegistrationWorkers {
-    @JobWorker(type = "check-age-worker") // Typ musi być taki sam jak w BPMN
-    public Map<String, Object> checkAge(@Variable(name = "age") Integer age) {
-        boolean isAdult = age != null && age >= 18;
-        return Map.of("isAdult", isAdult); // Zmienna wróci do procesu i ustawi bramkę
-    }
+
+
 }
