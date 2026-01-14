b8c666167071c23e7927045c37c5bc18f12d38f8
capSaymon <szymonpietruszka@onet.pl>
2026-01-14 20:37:35 +0100

    poprawki ReservationWorker oraz dodanie obsługi sprawdzania akcji klienta

--- Strzelnica/src/main/java/com/example/shootingrange/worker/ReservationWorker.java
+++ Strzelnica/src/main/java/com/example/shootingrange/worker/ReservationWorker.java
@@ -8,21 +8,36 @@ import java.util.Map;
 @Component
 public class ReservationWorker {
 
-    // Obsługuje zadanie wyszukiwania terminu
     @JobWorker(type = "searchDateToReservation")
     public Map<String, Object> searchDate() {
         boolean available = true;
-
+        System.out.println("Dostępna data rezerwacji: " + available);
         return Map.of("dateAvailable", available);
     }
 
     @JobWorker(type = "decideAboutReservation")
     public Map<String, Object> decideAboutReservation() {
-        return Map.of("accept", true);
+        boolean accept = true;
+        System.out.println("Akceptacja rezerwacji: " + accept);
+        return Map.of("accept", accept);
     }
 
     @JobWorker(type = "authorize-payment")
     public void authorizePayment() {
-        System.out.println("Autoryzacja płatności przebiegła pomyślnie.");
+        System.out.println("Pomyślna autoryzacja zapłaty za sesje");
+    }
+
+    @JobWorker(type = "check_positions_and_instructors")
+    public Map<String, Object> checkPositionsAndInstructors() {
+        boolean positionsAndInstructors = true;
+        System.out.println("Przydzielenie pozycji i instruktora: " + positionsAndInstructors);
+        return Map.of("positionsAndInstructors", positionsAndInstructors);
+    }
+
+    @JobWorker(type = "client_action_check")
+    public Map<String, Object> clientActionCheck() {
+        boolean clientActionAccepted = true;
+        System.out.println("Akcja klienta: " + clientActionAccepted);
+        return Map.of("clientActionAccepted", clientActionAccepted);
     }
 }
