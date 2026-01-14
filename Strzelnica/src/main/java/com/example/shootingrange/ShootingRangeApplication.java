9676993167f99e3b0fcb0eed9d564c4895bdc9ac
capSaymon <szymonpietruszka@onet.pl>
2026-01-14 18:59:52 +0100

    zainicjowanie klienta w głównym programie

--- Strzelnica/src/main/java/com/example/shootingrange/ShootingRangeApplication.java
+++ Strzelnica/src/main/java/com/example/shootingrange/ShootingRangeApplication.java
@@ -5,7 +5,6 @@ import io.camunda.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootApplication
@@ -19,19 +18,12 @@ public class ShootingRangeApplication {
     CamundaClient client = CamundaClient.newClient();
 
     // Dane wejściowe klienta przekazywane do procesów
-    Map<String, Object> variables = new HashMap<>();
-    variables.put("clientId", 2);
-    variables.put("clientAction", "reserve");
-    variables.put("age", 20);
-    variables.put("hasLicense", true);
-    variables.put("hasTraining", false);
-
+    Map<String, Object> variables = ClientData.getClientVariables();
 
     client.newCreateInstanceCommand()
             .bpmnProcessId("Process_0n3b101")
             .latestVersion()
             .variables(variables)
             .send();
-
   }
 }
