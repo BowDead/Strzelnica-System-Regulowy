0c0a37176bffc69de68783aa0153fbd1a119a348
capSaymon <szymonpietruszka@onet.pl>
2026-01-14 16:47:41 +0100

    sprawdzenie stanu rejestracji

--- Strzelnica/src/main/java/com/example/shootingrange/worker/RegistrationWorkers.java
+++ Strzelnica/src/main/java/com/example/shootingrange/worker/RegistrationWorkers.java
@@ -5,9 +5,29 @@ import io.camunda.client.annotation.Variable;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 public class RegistrationWorkers {
 
+    private static final AtomicInteger clientIdCounter = new AtomicInteger(0);
 
+    // Sprawdzenie stanu rejestracji klienta
+    @JobWorker(type = "Check_client_registration")
+    public Map<String, Object> checkClientRegistration(@Variable(name = "clientId") Integer clientId) {
+        boolean clientRegistered;
+        if (clientId == null) {
+            clientId = clientIdCounter.incrementAndGet();
+            clientRegistered = false;
+            System.out.println("Nowy klient [ID: " + clientId + "]");
+        } else {
+            clientRegistered = true;
+            System.out.println("Klient jest zarejestrowany [ID: " + clientId + "]");
+        }
+
+        return Map.of(
+                "clientRegistered", clientRegistered,
+                "clientId", clientId
+        );
+    }
 }
